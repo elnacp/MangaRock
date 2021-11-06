2d222c73dc262ef8e30c5d8de7393d9e0c3fce3a
elna <elnacp@gmail.com>
2021-11-06 19:06:18 +0100

    Firebase Included

--- Manga Rock/Assets/Scripts/FirebaseConnection.cs
+++ Manga Rock/Assets/Scripts/FirebaseConnection.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Firebase;
+using Firebase.Firestore;
 using Firebase.Database;
 using UnityEngine.UI;
 
@@ -12,17 +12,42 @@ public class FirebaseConnection : MonoBehaviour
     DatabaseReference reference;
     [SerializeField] InputField username_txt;
     [SerializeField] InputField email_txt;
+    [SerializeField] InputField nameToGet;
+
+
+    FirebaseFirestore db;
+    Dictionary<string, object> user;
 
 
     // Start is called before the first frame update
     void Start()
     {
         reference = FirebaseDatabase.DefaultInstance.RootReference;
+
+        db = FirebaseFirestore.DefaultInstance;
     }
 
     public void saveData()
     {
-        User user = new User();
+        user = new Dictionary<string, object>
+        {
+            {"Username", username_txt.text},
+            {"Email", email_txt.text}
+        };
+
+        db.Collection("Users").Document(username_txt.text).SetAsync(user).ContinueWith(task =>
+        {
+            if (task.IsCompleted)
+            {
+                Debug.Log("successfully added data to firebase");
+            }
+            else
+            {
+                Debug.Log("´not successfull");
+            }
+        });
+
+        /*User user = new User();
         user.username = username_txt.text;
         user.email = email_txt.text;
 
@@ -38,8 +63,29 @@ public class FirebaseConnection : MonoBehaviour
             {
                 Debug.Log("´not successfull");
             }
+        });*/
+    }
+
+    public void readData()
+    {
+        db.Collection("Users").Document(nameToGet.text).GetSnapshotAsync().ContinueWith(task =>
+        {
+            if(task.IsCompleted)
+            {
+                DocumentSnapshot snapshot = task.Result;
+                if(snapshot.Exists)
+                {
+                    user = snapshot.ToDictionary();
+                    foreach(KeyValuePair<string, object>pair in user)
+                    {
+                        Debug.Log(("{0}:{1}", pair.Key, pair.Value));
+                    }
+                }
+
+            }
         });
     }
+
 
     // Update is called once per frame
     void Update()
