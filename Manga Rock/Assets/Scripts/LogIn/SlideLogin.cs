2e1220dd5195e4eb72912148979213de0771cbb0
elna <elnacp@gmail.com>
2021-11-07 16:23:27 +0100

    Animations for Log in menu done

---
+++ Manga Rock/Assets/Scripts/LogIn/SlideLogin.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideLogin : MonoBehaviour
+{
+
+    public GameObject loginpanel;
+
+    public void ShowHideMenu()
+    {
+        if(loginpanel != null)
+        {
+            Animator animator = loginpanel.GetComponent<Animator>();
+            if(animator != null)
+            {
+                bool isOpen = animator.GetBool("show");
+                animator.SetBool("show", !isOpen);
+            }
+        }
+    }
+
+}
